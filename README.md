0fb0384686e2fb512951b3e67a8e4346330932dd
Diyah <me@hadiyah.me>
2015-09-16 09:56:36 -0700

    add barebone files for game

---
+++ README.md
@@ -0,0 +1,7 @@
+### Rock Paper Scissors Exercise
+1. Use the base html and js files as starter files <br/>
+2. When the user clicks a button as a choice, use ajax to retrieve the computer's choice <br/>
+3. Display the player and computer choice on the page. For example, 'You played Rock, I played Scissors'<br/>
+4. Show game results on page. For example, 'You win', or 'You lose', or 'Draw'  
+
+<img src='https://www.evernote.com/l/AOhvyNTiLpZNkbYNxRB5EFlEME3T86q_dN0B/image.png' />
---
+++ rps-game.js
@@ -0,0 +1,39 @@
+function newGame(){
+  $("#pieces-played").html("");  
+  $("#game-results").html("");
+  $("#new-game").css("display", "none");
+}
+
+function winner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice)
+    return "Draw";
+
+  if (playerChoice === "Paper") {
+    if (ComputerChoice === "Rock") {
+      return "You win";
+    } else if (computerChoice === "Scissors") {
+      return "You lose";
+    }
+  } else if (playerChoice === "Scissors") {
+    if (computerChoice === "Rock") {
+      return "You lose";
+    } else if (computerChoice === "Paper") {
+      return "You win";
+    }
+  } else if(playerChoice === "Rock") {
+    if (computerChoice === "Paper") {
+      return "You lose";
+    } else if (computerChoice === "Scissors") {
+      return "You win";
+    }
+  }
+}
+
+
+// 1. Add your code below these comments.
+// 2. Ensure that your code is ran after the DOM is ready
+// 3. Add Event Listeners to the buttons
+// 4. When the button is pushed use ajax and retrieve the computers choice from http://rock-paper-scissors-api.herokuapp.com/
+// 5. Display the players and the computers choice on the webpage
+// 6. Use the function winner to determine the winner and display results on the page
+// 7. Allow the user to play a new game
