4c6d7f9861db8a88e4fab6be9c3ad9207341bfe1
willis-meyers <s.willismeyers@gmail.com>
2015-09-16 21:20:54 -0700

    initial commit

--- rps-game.js
+++ rps-game.js
@@ -33,7 +33,20 @@ function winner(playerChoice, computerChoice) {
 // 1. Add your code below these comments.
 // 2. Ensure that your code is ran after the DOM is ready
 // 3. Add Event Listeners to the buttons
-// 4. When the button is pushed use ajax and retrieve the computers choice from http://rock-paper-scissors-api.herokuapp.com/
+// 4. When the button is pushed use ajax and retrieve the computers choice from
+// http://rock-paper-scissors-api.herokuapp.com/
 // 5. Display the players and the computers choice on the webpage
-// 6. Use the function winner to determine the winner and display results on the page
+// 6. Use the function winner to determine the winner and display results 
+//on the page
 // 7. Allow the user to play a new game
+
+$(document).ready(function(){
+  $('button').click(function() {
+      //var computerChoice = $.get("http://rock-paper-scissors-api.herokuapp.com/", function(data) {
+        //$("#pieces-played").html(data);
+      var playerChoice = $('#rock').click(function(){
+        $("#pieces-played").text("Rock");
+      })   
+    });
+
+  });
